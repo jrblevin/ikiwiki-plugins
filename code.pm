c78101c7ed8f90de54b54670a70c204dfb84048a
Jason Blevins <jrblevin@sdf.lonestar.org>
2008-11-23 10:03:18 -0500

    code: add perldoc documentation and explicitly license under GPL2+

--- code.pm
+++ code.pm
@@ -1,4 +1,8 @@
 #!/usr/bin/perl
+#
+# code -- a syntax highlighting plugin for Ikiwiki based on GNU
+# Source-highlight.
+
 package IkiWiki::Plugin::code;
 
 use warnings;
@@ -102,3 +106,40 @@ sub preprocess (@) {
 }
 
 1
+
+__END__
+
+=head1 NAME
+
+ikiwiki Plug-in: code
+
+=head1 SYNOPSIS
+
+Provides whole-file syntax highlighting as well as a preprocessor
+directive for syntax highlighting inline source code fragments.
+
+=head1 AUTHORS
+
+Jason Blevins <jrblevin@sdf.lonestar.org>, http://jblevins.org
+
+=head1 SEE ALSO
+
+http://jblevins.org/projects/ikiwiki/code
+
+=head1 LICENSE
+
+Copyright (C) 2008 Jason Blevins
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
